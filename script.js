3daea17c3877ae2de215746b7ecfdba3e7f8888f
thats-sarthak <sarthakmbd81@gmail.com>
2023-07-03 21:52:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function checkSum (){
+
+
+
+    var t1 = document.getElementById('t1').value;
+    var t2 = document.getElementById('t2').value;
+     var t5 =  parseInt(t1) + parseInt(t2);
+    var output = document.getElementById('result');
+    output.innerHTML = t5;
+     
+
+
+}
+
+
+// function checkSum() {
+//     alert("Hi")
+// }
